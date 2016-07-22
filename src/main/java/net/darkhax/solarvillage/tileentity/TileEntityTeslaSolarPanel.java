31340bd07d1eecd94dab545cc76a3d2cb6392ec5
Tyler Hancock <darklime@live.ca>
2016-07-22 16:37:10 -0600

    Solar panel now pushes power.

--- src/main/java/net/darkhax/solarvillage/tileentity/TileEntityTeslaSolarPanel.java
+++ src/main/java/net/darkhax/solarvillage/tileentity/TileEntityTeslaSolarPanel.java
@@ -8,7 +8,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
 
 import net.minecraftforge.common.capabilities.Capability;
-
+import net.darkhax.solarvillage.handler.SolarVillageConfig;
 import net.darkhax.tesla.capability.TeslaCapabilities;
 
 public class TileEntityTeslaSolarPanel extends TileEntity implements ITickable {
@@ -23,8 +23,15 @@ public class TileEntityTeslaSolarPanel extends TileEntity implements ITickable {
     @Override
     public void update () {
         
-        if (this.hasWorldObj() && !this.worldObj.provider.getHasNoSky() && this.worldObj.canBlockSeeSky(this.pos.offset(EnumFacing.UP)) && !this.worldObj.isRaining() && this.worldObj.getSkylightSubtracted() == 0 && this.container.getStoredPower() != this.container.getCapacity())
-            this.container.generatePower();
+        if (this.hasWorldObj()) {
+        	
+        	if (!this.worldObj.provider.getHasNoSky() && this.worldObj.canBlockSeeSky(this.pos.offset(EnumFacing.UP)) && !this.worldObj.isRaining() && this.worldObj.getSkylightSubtracted() == 0 && this.container.getStoredPower() != this.container.getCapacity())
+                this.container.generatePower();
+        	
+        	final TileEntity tile = this.getWorld().getTileEntity(this.getPos().offset(EnumFacing.DOWN));
+        	if (tile.hasCapability(TeslaCapabilities.CAPABILITY_CONSUMER, EnumFacing.UP))
+        		((ITeslaConsumer) tile.getCapability(TeslaCapabilities.CAPABILITY_CONSUMER, EnumFacing.UP)).givePower(Math.min(this.container.getStoredPower(), SolarVillageConfig.panelTransferRate), false);
+        }
     }
     
     @Override
